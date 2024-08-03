6f74eeb1288d9005cf9250f04ec98af49cd03069
John Emmerechts <john093e@gmail.com>
2024-08-03 21:59:55 +0200

    Readme

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -6,10 +6,10 @@ export default function HomePage() {
   return (
       <main className="container h-screen py-16">
         <div className="flex flex-col items-center justify-center gap-4 max-w-lg text-center w-full mx-auto ">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem] mb-2">
             Next Js using Zoid
           </h1>
-          <p>This website expose 2 Zoid components to the followinng website : <Link href="https://third-part-nextjs-zoid.vercel.app">third-part-nextjs-zoid.vercel.app</Link></p>
+          <p>This website expose 2 Zoid components used by the following website: <Link href="https://third-part-nextjs-zoid.vercel.app">third-part-nextjs-zoid.vercel.app</Link></p>
         </div>
       </main>
   );
